f3c1c92be73afd15e0df87284aff79b368e2672e
louhabali <louhabali@gmail.com>
2025-09-19 10:47:19 +0100

    profile in progress

--- 01-front/src/app/pages/register/register.component.ts
+++ 01-front/src/app/pages/register/register.component.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { HttpClientModule } from '@angular/common/http'; 
+
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, FormsModule,RouterModule,HttpClientModule],
+  imports: [CommonModule, FormsModule,RouterModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
