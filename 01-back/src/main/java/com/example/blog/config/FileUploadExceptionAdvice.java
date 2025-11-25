075e6b968410515db973831148431c31af4ca6a7
louhabali <louhabali@gmail.com>
2025-11-25 20:44:33 +0100

    adding a controlleradvice to catch large files exceptions and returns friendly errors

---
+++ 01-back/src/main/java/com/example/blog/config/FileUploadExceptionAdvice.java
@@ -0,0 +1,24 @@
+package com.example.blog.config;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+@ControllerAdvice
+public class FileUploadExceptionAdvice {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<String> handleMaxSizeException(MaxUploadSizeExceededException ex) {
+        return ResponseEntity
+                .badRequest()
+                .body("File is too large! Maximum allowed size is 500MB.");
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleOtherExceptions(RuntimeException ex) {
+        return ResponseEntity
+                .internalServerError()
+                .body("File upload failed: " + ex.getMessage());
+    }
+}
